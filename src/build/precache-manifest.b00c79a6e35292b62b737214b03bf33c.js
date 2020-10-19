2a7630fdfc60c8f47e76429632eaab5b45077f41
Lathashree01 <lathashreeharish01@gmail.com>
2020-10-19 23:13:03 +0530

    Initial Commit

---
+++ src/build/precache-manifest.b00c79a6e35292b62b737214b03bf33c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f85dc17b4f16cdfaeb75c092b111771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c90ab82ae500a906664",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "f23e57b3d241ada3648c",
+    "url": "/static/js/2.b0c3dd10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0c3dd10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c90ab82ae500a906664",
+    "url": "/static/js/main.bf227b02.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
